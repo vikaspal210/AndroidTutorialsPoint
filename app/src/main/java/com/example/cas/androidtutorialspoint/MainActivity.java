dd9b56ee16f94a28bc959a3728b5632fc9362897
Vikas Pal <pal.vikas210@gmail.com>
2018-09-26 20:39:41 +0530

    Added onClick for start and stop

--- app/src/main/java/com/example/cas/androidtutorialspoint/MainActivity.java
+++ app/src/main/java/com/example/cas/androidtutorialspoint/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.cas.androidtutorialspoint;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startService(View view) {
+        startService(new Intent(getBaseContext(),MyService.class));
+    }
+
+    public void stopService(View view) {
+        stopService(new Intent(getBaseContext(),MyService.class));
     }
 }
