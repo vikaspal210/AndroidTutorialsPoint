e772145c9e7ab4382901d8e31e3abb7029142f95
Vikas Pal <pal.vikas210@gmail.com>
2018-10-04 21:01:44 +0530

    Added URI's to PetContract for Content Provider

--- app/src/main/java/com/example/cas/androidtutorialspoint/data/PetContract.java
+++ app/src/main/java/com/example/cas/androidtutorialspoint/data/PetContract.java
@@ -1,12 +1,23 @@
 package com.example.cas.androidtutorialspoint.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public final class PetContract {
     public PetContract() {
     }
 
+    //content provider paths, authorities, base content
+    public static final String CONTENT_AUTHORITY="com.example.cas.androidtutorialspoint";
+    public static final Uri BASE_CONTENT_URI=Uri.parse("content://"+CONTENT_AUTHORITY);
+    public static final String PATH_PETS="pets";
+
     public static final class PetEntry implements BaseColumns{
+
+        //content uri
+        public static final Uri CONTENT_URI=Uri.withAppendedPath(BASE_CONTENT_URI,PATH_PETS);
+
+        //contract constants
         public final static String TABLE_NAME="pets";
         public final static String _ID=BaseColumns._ID;
         public final static String COLUMN_PET_NAME="name";
